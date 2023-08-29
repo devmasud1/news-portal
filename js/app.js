263d9c1fd9cfed017626a939f7d46ecb1bf9fcf2
devmasud1 <devmahmud.masu1@gmail.com>
2023-08-30 00:04:52 +0600

    basic structure

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const allNews = `https://openapi.programming-hero.com/api/news/categories`
+
+const newsCategory = ` https://openapi.programming-hero.com/api/news/category/{category_id}`
+
+
+const NewsDetail = `https://openapi.programming-hero.com/api/news/{news_id}`
+
+const MissingData = ` https://openapi.programming-hero.com/api/news/2e78e5e0310c2e9adbb6efb1a263e745`
